18ef4d13f8c15588f34f6c4d9ad18fdfb58b7141
Shakil Ahmmed <shakilfci461@gmail.com>
2019-03-30 19:09:11 +0600

    Design Change

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    details = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.title

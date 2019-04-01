0804c28d79f49e9497051a2ba688fea1caffde04
Shakil Ahmmed <shakilfci461@gmail.com>
2019-04-02 00:22:12 +0600

    Login & Logout

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login', views.login, name="login"),
     path('post_list', views.post_list, name="post_list"),
     path('update_post/<int:post_id>', views.update_post, name="update_post"),
     path('create_post', views.create_post, name="create_post"),
